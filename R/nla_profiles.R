a73c53add2be4084b540e8c095dcd2c2762be020
Luke Winslow <lawinslow@gmail.com>
2017-02-24 11:14:18 -0500

    Fix a small issue with profile data
    - Minor fixes

--- R/nla_profiles.R
+++ R/nla_profiles.R
@@ -6,6 +6,7 @@
 #'
 #'
 #' @import dplyr
+#' @importFrom lubridate mdy dmy
 #'
 #' @export
 nla_profiles = function(){
@@ -15,12 +16,15 @@ nla_profiles = function(){
   clean7 = prof7 %>% mutate(DATE=DATE_PROFILE, OXYGEN=DO_FIELD, PH=PH_FIELD, TEMP=TEMP_FIELD) %>%
     select(SITE_ID, DATE, DEPTH, OXYGEN, PH, TEMP)
 
+  clean7$DATE = lubridate::mdy(clean7$DATE)
+
   clean12 = prof12 %>% mutate(DATE=DATE_COL, TEMP=TEMPERATURE) %>% select(SITE_ID, DATE, DEPTH, OXYGEN, PH, TEMP)
+  clean12$DATE = lubridate::dmy(clean12$DATE)
 
   clean7$survey  = 2007
   clean12$survey = 2012
   bothdata = rbind(clean7, clean12)
-  cleanprof = bothdata[!is.na(bothdata$DEPTH), ]
+  cleanprof = subset(bothdata, !is.na(DEPTH) & !is.na(DATE))
 
   #there are some obvious outliers I am going to drop
   cleanprof = subset(cleanprof, TEMP < 40 & DEPTH < 200)
